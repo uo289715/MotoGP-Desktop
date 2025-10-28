b2bfe2a691e44353a36461e5b6db42cf26cf58b4
Javier Gutiérrez Esquinas <UO289715@uniovi.es>
2025-10-28 13:57:35 +0100

    Sesion 1 de js hecha

---
+++ js/memoria.js
@@ -0,0 +1,10 @@
+class Memoria {
+    constructor() {
+      // Constructor sin parámetros
+      
+    }
+
+    voltearCarta(carta) {
+        carta.setAttribute('data-estado', 'volteada');
+    }
+}
